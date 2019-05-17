7593a9d8d2f6169344d735bb21481714912965b3
ErissonCosta <erisson.costa.e@guararapes.ind.br>
2019-05-17 12:02:20 -0300

    Testes inicias

--- src/app/components/combinacoes/combinacoes.component.ts
+++ src/app/components/combinacoes/combinacoes.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'gua-ficha-combinacoes',
+  selector: 'gua-ficha-combinacoes-tema',
   templateUrl: './combinacoes.component.html',
   styleUrls: ['./combinacoes.component.scss']
 })
